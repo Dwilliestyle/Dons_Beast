c15b19464da0f992c3b602233c820563f07941f5
Dwilliestyle <dwilliestyle@gmail.com>
2026-03-02 17:10:02 -0500

    Added a launch file to move the robot around

---
+++ beast_bringup/launch/movement.launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='beast_bringup',
+            executable='esp32_bridge.py',
+            name='esp32_bridge',
+            output='screen'
+        ),
+        Node(
+            package='beast_controller',
+            executable='keyboard_ctrl.py',
+            name='keyboard_ctrl',
+            output='screen'
+        ),
+    ])
