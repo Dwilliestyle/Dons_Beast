50e4f9fd4224243555e3e4985b724dc6b3dc788c
Dwilliestyle <dwilliestyle@gmail.com>
2026-03-03 17:05:26 -0500

    correct movement launch file

--- beast_bringup/launch/movement.launch.py
+++ beast_bringup/launch/movement.launch.py
@@ -5,19 +5,19 @@ def generate_launch_description():
     return LaunchDescription([
         Node(
             package='beast_bringup',
-            executable='esp32_bridge.py',
+            executable='esp32_bridge',
             name='esp32_bridge',
             output='screen'
         ),
         Node(
             package='beast_controller',
-            executable='keyboard_ctrl.py',
+            executable='keyboard_ctrl',
             name='keyboard_ctrl',
             output='screen'
         ),
         Node(
             package='beast_utils',
-            executable='battery_monitor.py',
+            executable='battery_monitor',
             name='battery_monitor',
             output='screen'
         ),
